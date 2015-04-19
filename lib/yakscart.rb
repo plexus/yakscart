6e63066f6364f2565a47b2e3e034a69d2bf6069b
Arne Brasseur <arne@arnebrasseur.net>
2015-04-19 18:20:15 +0200

    First endpoint, model, mapper

--- lib/yakscart.rb
+++ lib/yakscart.rb
@@ -3,5 +3,35 @@ require "yaks-html"
 
 module Yakscart
   class Application < Sinatra::Base
+    register Sinatra::Yaks
+    helpers Sinatra::YaksHelpers
+
+    configure_yaks do
+      mapper_namespace Yakscart
+    end
+
+    get '/' do
+      yaks Product.all
+    end
+  end
+
+  class Product < Sequel::Model
+  end
+
+  class ProductMapper < Yaks::Mapper
+    attributes :id, :name, :price
+
+    def price
+      object.price / 100
+    end
+
+    form :add_to_cart do
+      action "/cart/line_items"
+      method "POST"
+      media_type "application/x-www-form-urlencoded"
+
+      hidden :product_id, value: ->{ object.id }
+      number :quantity, value: 0
+    end
   end
 end
